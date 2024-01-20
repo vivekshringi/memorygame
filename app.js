e0b64968b3b9a9b15905993fa77d3a4bd5166292
vivekshringi <shringi.vivek@gmail.com>
2024-01-20 23:04:07 +0100

    memorygame

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+function shuffleArray(arr) {
+  let i = arr.length, j, temp;
+  while(--i > 0){
+    j = Math.floor(Math.random()*(i+1));
+    temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
+}
+
+function getArray(){
+  const numbers = [];
+  for (let index = 0; index < 16; index++) {
+      numbers[index] = parseInt(index/2);
+  }
+  return numbers
+}
+
+app.get('/numbers', (req, res) => {
+  let numbers = getArray();
+  let finalArray = shuffleArray(numbers);
+  res.send({"numbers":finalArray})
+})
+
+app.use('/static', express.static('public'));
+
+app.post('/', (req, res) => {
+    
+    res.send('Got a POST request')
+  })
+
+app.put('/user', (req, res) => {
+    res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', (req, res) => {
+    res.send('Got a DELETE request at /user')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ public/javascripts/game.js
@@ -0,0 +1,64 @@
+let firstValue;
+let secondValue;
+let count = 0;
+let match = 0;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getArray(){
+fetch("/numbers")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    return response.json();
+  }).then((res)=>{
+    let random = getRandomInt(3);
+    for (let index = 0; index < res.numbers.length ; index++) {
+    let block = document.querySelector(`[id='${index}']`)
+    let image = document.createElement("img");
+    image.src = `/static/images/${res.numbers[index]}${random}.svg`;
+    image.setAttribute("id",`image${index}`);
+    block.appendChild(image);
+    block.addEventListener('click', () => {
+       count++;
+       document.getElementById('count').innerText=count;
+       block.firstChild.style.display="block";
+      if(firstValue==undefined){
+        firstValue = block.firstChild.src;
+        firstBlock = block.firstChild;
+        console.log("firstValue "+firstValue);
+        return;
+      }
+      if(secondValue==undefined){
+        secondValue = block.firstChild.src;
+        secondBlock = block.firstChild;
+        console.log("SecondValue "+secondValue);
+      }
+      if(firstValue==secondValue){
+        console.log("Wonderful");
+        match++;
+        document.getElementById('match').innerText=match;
+        firstValue = undefined;
+        secondValue = undefined;
+      }
+      else{
+        setTimeout(() => {
+          firstBlock.style.display="none";
+          secondBlock.style.display="none";
+          firstValue = undefined;
+          secondValue = undefined;
+        }, "1000");
+
+      }
+    });
+  }
+  })
+  .catch((err) => console.error(`Fetch problem: ${err.message}`));
+}
+
+getArray();
+
+
